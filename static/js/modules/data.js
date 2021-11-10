83eca4894999b77bbbfd23329b19779fb202a653
Daniel van de Velde <velde.van.de.daniel@gmail.com>
2021-11-10 18:56:26 +0100

    removed global var

--- static/js/modules/data.js
+++ static/js/modules/data.js
@@ -1,11 +1,13 @@
 import { update } from "./updateMap.js"
 let dataArray = []
-const projection = d3.geoMercator().center([0, 5]).scale(150)
 
 export let data = {
   cleanData: rawData => {
+    const projection = d3.geoMercator().center([0, 5]).scale(150)
+
     dataArray.push(rawData)
     dataArray.length >= 3 ? dataArray.shift() : null
+
     if (dataArray.length >= 2) {
       const angle = {
         lat: projection([dataArray[1].longitude, dataArray[1].latitude])[0],
