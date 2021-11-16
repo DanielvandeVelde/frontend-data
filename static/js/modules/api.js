6dc0f1740d157954ea97838f990bd73716f665ee
Daniel van de Velde <velde.van.de.daniel@gmail.com>
2021-11-16 11:12:07 +0100

    remove async from function

--- static/js/modules/api.js
+++ static/js/modules/api.js
@@ -1,7 +1,7 @@
 import { data } from "./data.js"
 
 export let api = {
-  fetch: async () => {
+  fetch: () => {
     fetch("https://api.wheretheiss.at/v1/satellites/25544")
       .then(response => response.json())
       .then(rawData => {
